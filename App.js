0d8cdcd74041d5b03568c88c8f04ed41787ebbf7
Manmohan Kushwaha <mkush575@gmail.com>
2026-05-21 21:19:16 +0530

    Add console log for commit activity tracking

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ const heading= React.createElement(
      {id: "heading"},
       "Namaste React"
     );
-
+console.log("testing commmit activity");
 const parent= React.createElement(
     "div",
     {id:"parent"},
