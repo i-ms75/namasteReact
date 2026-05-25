06a5d898203b6317e901f60b42c1995cb638faa4
Manmohan Kushwaha <mkush575@gmail.com>
2026-05-25 13:43:18 +0530

    random css

--- App.js
+++ App.js
@@ -1,44 +1,113 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// import logo from './src/img/logo.jpg';
 
-// const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
+/*
+everything in here is a comment for this application
+Building a swiggy clone with react
+- Heaader
+    - Navbar
+    - Logo
+    - Search    
+- Body
+    - Restaurant Container
+        - Restaurant Card
+            - Image
+            - Name
+            - Cuisines
+            - Rating
+- Footer
+*/
 
+const Header = () =>
+{
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img 
+                className="logo" 
+                src="https://res.cloudinary.com/cloudinary-marketing/images/c_scale,w_auto,dpr_auto/f_auto,q_auto/v1750375701/basspro-case_study-image2/basspro-case_study-image2.png?_i=AA" 
+                />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
 
-// JSX - jsx is a syntax extension to js, it is not html, it is not string, it is jsx, it is a syntax extension to js which looks like html but it is not html, it is not string, it is jsx, it is a syntax extension to js which looks like html but it is not html, it is not string, it is jsx, it is a syntax extension to js which looks like html but it is not html, it is not string, it is jsx, it is a syntax extension to js which looks like html but it is not html, it is not string, it is jsx, it is a syntax extension to js which looks like html but it is not html, it is not string, it is jsx, it is a syntax extension to js which looks like html but it is not html, it is not string, it is jsx, it is a syntax extension to js which looks like html but it is not html, it is not string, it is jsx, it is a syntax extension to js which looks like html but it is not html, it is not string, it is jsx, it is a syntax extension to js which looks like html but it is not html, it is not string.
-const jsxHeading= <h1 id="heading">Namaste React using JSX</h1>
+        </div>
+    );
+};
 
-
-// React Component - it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx.
-// Class based component - it is a class which extends React.Component and has a render method which returns jsx, it is a class which extends React.Component and has a render method which returns jsx, it is a class which extends React.Component and has a render method which returns jsx, it is a class which extends React.Component and has a render method which returns jsx, it is a class which extends React.Component and has a render method which returns jsx, it is a class which extends React.Component and has a render method which returns jsx, it is a class which extends React.Component and has a render method which returns jsx, it is a class which extends React.Component and has a render method which returns jsx, it is a class which extends React.Component and has a render method which returns jsx, it is a class which extends React.Component and has a render method which returns jsx, it is a class which extends React.Component and has a render method which returns jsx, it is a class which extends React.Component and has a render method which returns jsx, it is a class which extends React.Component and has a render method which returns jsx.
-// Functional component - it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx, it is a function which returns jsx.
-
-const HeaderComponent=()=>{
-    return <h1 id="heading">Namaste React heading Component with return statemnet</h1>
+const ResturantCard = () =>
+{
+    return (
+        <div className="resturant-card">
+            <img className="resturant-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/FOOD_CATALOG/IMAGES/CMS/2024/8/3/4212e047-b31c-4448-bc23-c2330abeeb37_f81c1236-0b5f-4b60-b220-7722c3878f3c.jpg" alt="resturant logo" />
+            <h5>Nepali, North Indian, South Indian</h5>
+            <h5>4 star</h5>
+            <h4>Biriyani zone</h4>
+        </div>
+    )
 }
 
-const HeaderComponentWithArrowFunction=()=><h2 id="heading">Namaste React heading Component with Arrow Function</h2>
-const Title=()=> <h3>Namaste React Title Component</h3>
-const elem=<h4>Namaste React h4 element</h4>
+const Body = () =>
+{
+    return(
+        <div className="body">
+            <div className="search"> Search </div> 
+            {/* //Restaurant Card */}
+            <div className="resturant-container">
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+                <ResturantCard/>
+            </div>
+        </div>
+    )
+}
 
-const BodyComponentWithMultipleLinesOfJSX=()=>(
-    <div id="container">
-        <HeaderComponent />
-        <HeaderComponentWithArrowFunction />
-        {Title()}
-        {elem}
-        <h3 id="body">Namaste React  body Component with component composition</h3>
-    </div>
-);
-
-
-
-
-
-
+const AppLayout = () => 
+{
+    return (
+        <div className="app">
+            <Header />
+            <Body/>
+        </div>
+    );
+    
+};
 
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<BodyComponentWithMultipleLinesOfJSX />);
+root.render(<AppLayout />);
 
 
