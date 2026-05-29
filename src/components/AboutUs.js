bb26d45a097f525b4172d167c693670071e2969b
Manmohan Kushwaha <mkush575@gmail.com>
2026-05-29 20:46:56 +0530

    Add AboutUs, Contact, and Error components

---
+++ src/components/AboutUs.js
@@ -0,0 +1,11 @@
+const AboutUs = () => 
+{
+    return (
+        <div>
+            <h1>Heyy there here's about me, A struggling Software Engineer!!</h1>
+            <h2>struggling to keep up with inflation</h2>
+        </div>
+    )
+}
+
+export default AboutUs;
