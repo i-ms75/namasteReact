aade9d0e585c4d9739dcd13e0245b0355afdf49e
Manmohan Kushwaha <mkush575@gmail.com>
2026-06-01 21:26:32 +0530

    Add state management to User and UserClass components

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 const User = (props) =>
 {
+    const [count]=useState(0);
     return ( <div className="user-card">
+            <h2>Count: {count}</h2>
             <h2>Name: {props.name}</h2>
             <h3>Location: {props.location}</h3>
             <h4>Contact: sudodeveloper.com</h4>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,13 +4,20 @@ class UserClass extends React.Component
     constructor(props)
     {
         super(props);
+        this.state={
+            count:0,
+            count2:2
+        }
 
     }
     render()
     {
         const {name,location}=this.props;
+        const {count,count2}=this.state
         return ( 
         <div className="user-card">
+            <h2>Count: {count}</h2>
+            <h2>Count2: {count2}</h2>
             <h2>Name: {name}</h2>
             <h3>Location: {location}</h3>
             <h4>Contact: sudodeveloper.com</h4>
